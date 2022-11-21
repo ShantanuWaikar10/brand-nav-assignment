c5c3ad43c21e6a3d908b8a646553c6ee61da8dbe
ShantanuWaikar10 <89289274+ShantanuWaikar10@users.noreply.github.com>
2022-11-21 18:47:41 +0530

    commit

--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -5,32 +5,31 @@ import './Features.css'
 
 const Features = () => {
   return (
-      <div className='features__container' id='features'>
+    <div className='features__container' id='features'>
       <div className="features__container__img">
-
         <img src={Illustration2} alt="" />
       </div>
-        <div className="features__texts">
-          <h2>We Provide Many Features You Can Use</h2>
-          <p className='features__texts__p'>You can explore the features that we provide with fun and have their own functions each feature.</p>
-          <div className="icon__text">
-            <img src={Group1120} alt="" />
-            <p>Powerfull online protection.</p>
-          </div>
-          <div className="icon__text">
-            <img src={Group1120} alt="" />
-            <p>Internet without borders.</p>
-          </div>
-          <div className="icon__text">
-            <img src={Group1120} alt="" />
-            <p>Supercharged VPN</p>
-          </div>
-          <div className="icon__text">
-            <img src={Group1120} alt="" />
-            <p>No specific time limits.</p>
-          </div>
+      <div className="features__texts">
+        <h2>We Provide Many Features You Can Use</h2>
+        <p className='features__texts__p'>You can explore the features that we provide with fun and have their own functions each feature.</p>
+        <div className="icon__text">
+          <img src={Group1120} alt="" />
+          <p>Powerfull online protection.</p>
+        </div>
+        <div className="icon__text">
+          <img src={Group1120} alt="" />
+          <p>Internet without borders.</p>
+        </div>
+        <div className="icon__text">
+          <img src={Group1120} alt="" />
+          <p>Supercharged VPN</p>
+        </div>
+        <div className="icon__text">
+          <img src={Group1120} alt="" />
+          <p>No specific time limits.</p>
         </div>
       </div>
+    </div>
   )
 }
 
